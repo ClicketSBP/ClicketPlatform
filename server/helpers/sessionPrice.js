cc9660cc4220a62e5f0727d7d6501aa30723677a
Jeffrey <descanjeffrey@gmail.com>
2017-01-29 02:44:00 +0100

    Helpers SessionPrice: rewrite to return 0 when session is not active

--- server/helpers/sessionPrice.js
+++ server/helpers/sessionPrice.js
@@ -2,18 +2,32 @@ const config = require('../../config/clicket.config'),
     moment = require('moment');
 
 const sessionPrice = (() => {
-    const calculatePrice = (started_on, stopped_on, pricePerHour, cb) => {
-        let timeDifference = moment(stopped_on).diff(moment(started_on), 'hours', true);
-        let parkCosts = timeDifference * pricePerHour;
+    const calculatePrice = (ses, cb) => {
         let data = {
             time: {
-                hoursParked: timeDifference,
-                minutesParked: timeDifference * 60
+                hoursParked: 0,
+                minutesParked: 0
             },
             price: {
-                'parkCosts': parkCosts,
-                'transactionCosts': config.prices.costsPerTransaction,
-                'total': (parkCosts + config.prices.costsPerTransaction)
+                'parkCosts': 0,
+                'transactionCosts': 0,
+                'total': 0
+            }
+        }
+
+        if (!ses.active) {
+            let timeDifference = moment(ses.stopped_on).diff(moment(ses.started_on), 'hours', true);
+            let parkCosts = timeDifference * ses.zone_id.price;
+            data = {
+                time: {
+                    hoursParked: timeDifference,
+                    minutesParked: timeDifference * 60
+                },
+                price: {
+                    'parkCosts': parkCosts,
+                    'transactionCosts': config.prices.costsPerTransaction,
+                    'total': parkCosts + config.prices.costsPerTransaction
+                }
             }
         }
 
